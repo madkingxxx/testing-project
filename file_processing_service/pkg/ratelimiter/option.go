5929af8dd1f97c92a8d00d8e514d7623b96c608e
madkingxxx <otabek2770530@gmail.com>
2023-02-15 23:27:42 +0500

    edited limiter

--- file_processing_service/pkg/ratelimiter/option.go
+++ file_processing_service/pkg/ratelimiter/option.go
@@ -1,9 +1,33 @@
 package ratelimiter
 
+import "time"
+
 type Option func(*limiter)
 
+// WithMaxConcurrentConnections is an option that sets the maximum number of concurrent connections.
 func WithMaxConcurrentConnections(maxConcurrentConnections int) Option {
 	return func(l *limiter) {
 		l.maxLimit = maxConcurrentConnections
+	}
+}
+
+// WithLimitingByUserID is an option that enables limiting by user ID.
+func WithLimitingByUserID() Option {
+	return func(l *limiter) {
+		l.isLimitedByID = true
+	}
+}
+
+// WithDuration is an option that sets the duration for which the limit is applied.
+func WithDuration(duration time.Duration) Option {
+	return func(l *limiter) {
+		l.duration = duration
+	}
+}
+
+// WithLimiterType is an option that sets the type of limiter.
+func WithLimiterType(limiterType limiterType) Option {
+	return func(l *limiter) {
+		l.limiterType = limiterType
 	}
 }
--- file_processing_service/pkg/ratelimiter/ratelimiter.go
+++ file_processing_service/pkg/ratelimiter/ratelimiter.go
@@ -2,44 +2,105 @@ package ratelimiter
 
 import (
 	"errors"
+	"fmt"
 	"sync"
+	"time"
 )
 
-var ErrLimitExceeded = errors.New("request limit exceeded")
+var (
+	ErrLimitExceeded      = errors.New("request limit exceeded")
+	ErrWrongFunctionUsage = errors.New("wrong function usage by limiter option")
+)
 
-const DefaultMaxLimit = 10
+const (
+	_defaultMaxLimit    = 10
+	_defaultDuration    = 1 * time.Second
+	_defaultLimiterType = Concurrent
+
+	_defaultKey = "default"
+)
 
 type limiter struct {
-	mu         sync.Mutex
-	maxLimit   int
-	concurrent int
+	mu            sync.Mutex
+	maxLimit      int
+	concurrent    map[string]int
+	isLimitedByID bool
+	limiterType   limiterType
+	duration      time.Duration
 }
+
+type limiterType int
+
+const (
+	_ limiterType = iota
+	Concurrent
+	DurationBased
+)
 
 func (l *limiter) Acquire() error {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 
-	if l.concurrent >= l.maxLimit {
+	if l.concurrent[_defaultKey] >= l.maxLimit {
 		return ErrLimitExceeded
 	}
 
-	l.concurrent++
+	l.concurrent[_defaultKey]++
+
+	return nil
+}
+
+func (l *limiter) AcquireWithID(id string) error {
+	if !l.isLimitedByID {
+		return ErrWrongFunctionUsage
+	}
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.concurrent[id] >= l.maxLimit {
+		return fmt.Errorf("%w for - %s", ErrLimitExceeded, id)
+	}
+
+	l.concurrent[id]++
+
 	return nil
 }
 
 func (l *limiter) Release() {
 	l.mu.Lock()
-	defer l.mu.Unlock()
+	l.concurrent[_defaultKey]--
+	l.mu.Unlock()
+}
 
-	l.concurrent--
+func (l *limiter) ReleaseWithID(id string) {
+	l.mu.Lock()
+	l.concurrent[id]--
+	l.mu.Unlock()
+}
+
+func (l *limiter) Start() {
+	if l.limiterType == DurationBased {
+		ticker := time.NewTicker(l.duration)
+		go func() {
+			for range ticker.C {
+				l.mu.Lock()
+				l.concurrent = make(map[string]int)
+				l.mu.Unlock()
+			}
+		}()
+	}
 }
 
 func NewLimiter(options ...Option) *limiter {
 	limiter := &limiter{
-		maxLimit: DefaultMaxLimit,
+		maxLimit:    _defaultMaxLimit,
+		duration:    _defaultDuration,
+		limiterType: _defaultLimiterType,
+		concurrent:  make(map[string]int),
 	}
 	for _, option := range options {
 		option(limiter)
 	}
+	limiter.Start()
 	return limiter
 }
